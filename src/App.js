310adf4829d0bcb5543c5ef218d82d135d714505
ErmiasTsege <71066745+ErmiasTsege@users.noreply.github.com>
2020-12-13 01:12:40 -0600

    Netflix-React First Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import requests from './request';
+import Row from "./ROW/Row"
+import Banner from"./Banner/Banner"
+import Nav from "./Nav/Nav"
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
+       isLargeRow
+        />
+            <Row title="Trending Now"  fetchUrl={requests.fetchTrending} />
+            <Row title="Top Rated" fetchUrl={requests.fetchNetflixOriginals}/>
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+            <Row title="Comedy Movies"  fetchUrl={requests.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+            <Row title="Romance Movies"  fetchUrl={requests.fetchRomanceMovies} />
+            <Row title="Documentry Movies"  fetchUrl={requests.fetchDocumentaries} />
+      <Row />
+    </div>
+  );
+}
+
+export default App;
